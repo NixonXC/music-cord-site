68fd35c6290d3e8833037de8b668887bb16845fa
NixonXC <81410474+NixonXC@users.noreply.github.com>
2022-02-27 21:32:44 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -3,5 +3,6 @@ function copyToClipboard(element) {
     $("body").append($temp);
     $temp.val($(element).text()).select();
     document.execCommand("copy");
+    window.alert("Copied the text: " + $temp.val());
     $temp.remove();
 }
